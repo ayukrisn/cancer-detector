bae15e3882cde829a327a95fcc907037bba46b9e
ayuukrisn <ayuukrisn@gmail.com>
2024-11-04 23:46:15 +0800

    added crop image

--- app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.dicoding.asclepius.view
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -15,6 +16,8 @@ import com.dicoding.asclepius.R
 import com.dicoding.asclepius.databinding.ActivityMainBinding
 import com.dicoding.asclepius.helper.ImageClassifierHelper
 import org.tensorflow.lite.task.vision.classifier.Classifications
+import com.yalantis.ucrop.UCrop
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -69,12 +72,39 @@ class MainActivity : AppCompatActivity() {
     ) { uri: Uri? ->
         if (uri != null) {
             currentImageUri = uri
-            showImage()
+            startCrop(uri) //Crop the image
         } else {
             Log.d("Photo Picker", "No media selected")
         }
     }
 
+    /***
+     * Function to crop the image
+     */
+    private fun startCrop(uri: Uri) {
+        val destinationUri = Uri.fromFile(File(cacheDir, "cropped_image.jpg"))
+        UCrop.of(uri, destinationUri)
+            .withAspectRatio(1f, 1f)
+            .withMaxResultSize(500, 500)
+            .start(this)
+    }
+
+    // Handle result from uCrop
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+            currentImageUri = UCrop.getOutput(data!!)
+            showImage() // Display the cropped image
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            val cropError = UCrop.getError(data!!)
+            showToast("Crop error: ${cropError?.message}")
+        }
+    }
+
+    /***
+     * Show image chosen
+     */
     private fun showImage() {
         currentImageUri?.let {
             Log.d("Image URI", "showImage: $it")
@@ -116,7 +146,7 @@ class MainActivity : AppCompatActivity() {
     private fun moveToResult(results: List<Classifications>) {
         val intent = Intent(this, ResultActivity::class.java)
 
-        // Prepare the classification results to pass to ResultActivity
+        // Prepare the classification results to be passed to ResultActivity
         val classifications = results.firstOrNull()?.categories?.map {
             "${it.label} : ${it.score * 100}%"
         } ?: listOf("No results")
